032eb911225f44bd14b3b74e23573bf5fc2e6eb6
René Leban <leban.rene@gmail.com>
2016-08-19 08:34:07 +0200

    add jwt, add simple logger, first token test

--- board/core.py
+++ board/core.py
@@ -1,43 +1,57 @@
 # -*- coding: utf-8 -*-
 import json
+import configparser
+import logging
+import uuid
 
 from bottle import Bottle, run, response
+from jose import jwt
 
-# Location of the Data-File !
-BOARD_DATA_JSON = '/tmp/board-data.json'
+
+# read config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+#configure logging
+logging.basicConfig(filename='board.log',level=logging.DEBUG)
 
 storagelist = []
+accesslist = []
 
 app = Bottle()
-
 
 @app.get('/')
 def getinfo():
     return "<html><head></head><body>" \
-           "<p>GET: <strong>/board</strong> - list all boards</p>" \
-           "<p>PUT: <strong>/board/[a-z]</strong> - add board</p>" \
-           "<p>DELETE: <strong>/board/[0-9]</strong> - delete by ID</p>" \
-           "<p>DELETE: <strong>/board/[a-zA-Z\s]</strong> - delete by name</p>" \
+           "<p>GET: <strong>/board/token</strong> - list all boards</p>" \
+           "<p>PUT: <strong>/board/[a-z]/token</strong> - add board</p>" \
+           "<p>DELETE: <strong>/board/[0-9]/token</strong> - delete by ID</p>" \
            "</body></html>"
 
 
-@app.get('/board')
-@app.get('/board/')
-def getrequest():
+@app.get('/board/<token>')
+def getrequest(token):
+    logging.debug('token: '+token)
+    userdata = jwt.decode(token, config['board']['secret'], algorithms=['HS256'])
+    logging.debug('user: '+userdata["uuid"])
     restore()
     return listallboards(response)
 
 
-@app.put('/board/<name:re:[a-zA-Z\s]*>')
-def postrequest(name):
+@app.put('/board/<token>/<name:re:[a-zA-Z\s]*>')
+def postrequest(name, token):
+    logging.debug('token: '+token)
+    userdata = jwt.decode(token, config['board']['secret'], algorithms=['HS256'])
     restore()
-    return addboard(response, name)
+    return addboard(userdata['uuid'], response, name)
 
 
-@app.delete('/board/<id:int>')
-def deletebyidrequest(id):
+@app.delete('/board/<token>/<board_id>')
+def deletebyidrequest(board_id, token):
+    logging.debug('token: '+token)
+    userdata = jwt.decode(token, config['board']['secret'], algorithms=['HS256'])
     restore()
-    return removeboard(response, int(id))
+    return removeboard(userdata['uuid'], response, board_id)
 
 
 def listallboards(response):
@@ -46,40 +60,67 @@ def listallboards(response):
 
 
 def restore():
-    global storagelist
+    global storagelist, accesslist
     if len(storagelist) is 0:
         try:
-            with open(BOARD_DATA_JSON, 'r') as f:
+            with open(config['board']['storagefile'], 'r') as f:
                 storagelist = json.load(f)
         except FileNotFoundError:
-            print("WARNING: data-file not found (" + BOARD_DATA_JSON + ")")
+            logging.info("file not found (" + config['board']['storagefile'] + ")")
+
+    if len(accesslist) is 0:
+        try:
+            with open(config['board']['acl_storagefile'], 'r') as f:
+                accesslist = json.load(f)
+        except FileNotFoundError:
+            logging.info('File not found: '+config['board']['acl_storagefile'])
 
 
-def addboard(response, name):
-    id = len(storagelist) + 1
-    new_board = {'id': id, 'name': name}
+def addboard(user_uuid, response, name):
+    global accesslist, storagelist
+
+    board_uuid = uuid.uuid4()
+
+    new_board = {'id': str(board_uuid), 'name': name}
+    new_access = {'board_id' : str(board_uuid), 'user_id' : str(user_uuid)}
+
     storagelist.append(new_board)
+    accesslist.append(new_access)
 
-    with open(BOARD_DATA_JSON, 'w') as f:
-        json.dump(storagelist, f)
+    updateStorage()
 
     response.content_type = 'application/json; charset=utf-8'
     return json.dumps(new_board)
 
 
-def removeboard(response, id):
-    count = len(storagelist)
-    list = [x for x in storagelist if x['id'] == int(id)]
-    for item in list:
-        storagelist.remove(item)
+def removeboard(user_uuid, response, board_id):
+    global accesslist, storagelist
 
-    with open(BOARD_DATA_JSON, 'w') as f:
-        json.dump(storagelist, f)
+    count = len(storagelist)
+
+    list = [x for x in storagelist if x['id'] == str(board_id)]
+    removed_boards = []
+
+    for item in list:
+        for acl in accesslist:
+            if acl['board_id'] == str(item['id']) and acl['user_id'] == str(user_uuid):
+                storagelist.remove(item)
+
+    new_access_list = [acl for acl in accesslist if acl['board_id'] != str(board_id)]
+    accesslist = new_access_list;
+
+    updateStorage()
 
     response.content_type = 'application/json; charset=utf-8'
     return json.dumps({'message': count > len(storagelist)})
 
+def updateStorage():
+    with open(config['board']['storagefile'], 'w') as f:
+        json.dump(storagelist, f)
+
+    with open(config['board']['acl_storagefile'], 'w') as f:
+        json.dump(accesslist, f)
 
 # prevent running with nosetests
 if __name__ == '__main__':
-    run(app, host='localhost', port=8080, debug=True, server='cherrypy')
+    run(app, host=config['board']['host'], port=config['board']['port'], debug=True, server='cherrypy')
--- tests/test_board.py
+++ tests/test_board.py
@@ -7,6 +7,7 @@ from webtest import TestApp
 
 # mock for response
 TESTBOARD = "testboard"
+TEST_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZjRkMzkxOTItZmFhZC00NDU4LThhNjktYzE0ZmJiZDQ0N2E0IiwibmFtZSI6IkFybm8gTnltb3VzIiwiYWRtaW4iOnRydWV9.S5pchdBRgdxk5Qk_SKgZQnuFQCNyGN5EHSDIxLPpirY"
 
 test_app = TestApp(app)
 
@@ -18,29 +19,24 @@ class response:
 
 
 class TestJSON(unittest.TestCase):
-    def test_list_all(self):
-        response = test_app.get('/board')
-        self.assertEqual(response.status_int, 200)
-        self.assertEqual(response.status, "200 OK")
-        self.assertEqual(response.content_type, "application/json")
 
     def test_add_item(self):
         global id
-        response = test_app.put('/board/' + TESTBOARD)
+        response = test_app.put('/board/' + TEST_TOKEN + '/' + TESTBOARD)
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, "application/json")
         self.assertEqual(response.json['name'], TESTBOARD)
         id = response.json['id']
 
-    def test_list_all_with_trailing(self):
-        response = test_app.get('/board/')
+    def test_list_all(self):
+        response = test_app.get('/board/' + TEST_TOKEN )
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.status, "200 OK")
         self.assertEqual(response.content_type, "application/json")
 
     def test_delete_item(self):
         self.assertNotEqual(id, 0)
-        response = test_app.delete('/board/' + str(id))
+        response = test_app.delete('/board/' + TEST_TOKEN + '/' + str(id))
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, "application/json")
 
