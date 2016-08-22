9dcf53995b08fea99e80b071147e8e3440f5ff5e
Jens Sproede <jens.sproede@gmail.com>
2016-08-22 09:55:33 +0200

    List-Service: Save owner for access control

--- list/core.py
+++ list/core.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from bottle import Bottle, run, response
+from jose import jwt
 import json, uuid, configparser, os, dataset
 
 config = configparser.ConfigParser()
@@ -9,62 +10,72 @@ LOCATION_DATA = "sqlite:///" + config['list']['storage_location'] + os.path.sep 
 
 
 class List(object):
-    def __init__(self, list_id, board_id, name):
+    def __init__(self, list_id, board_id, name, owner):
         self.board_id = board_id
         self.list_id = list_id
         self.name = name
+        self.owner = owner
 
     def dump(self):
         return {
             'board_id': self.board_id,
             'list_id': self.list_id,
-            'name': self.name
+            'name': self.name,
+            'owner': self.owner
         }
 
 
 app = Bottle()
 
 
-@app.get('/lists/list/<board_id>')
-def get_all_elements(board_id):
-    return lists_for_board(response, board_id)
+@app.get('/lists/list/<token>/<board_id>')
+def get_all_elements(token, board_id):
+    userdata = extract_userdata(token)
+    return lists_for_board(response, userdata["uuid"], board_id)
 
 
-@app.delete('/lists/list/<list_id>')
-def remove(list_id):
-    return remove_list(response, list_id)
+@app.delete('/lists/list/<token>/<list_id>')
+def remove(token, list_id):
+    userdata = extract_userdata(token)
+    return remove_list(response, userdata["uuid"], list_id)
 
 
-@app.put('/lists/board/<board_id>/<name>')
-def add_list_to_board(board_id, name):
-    return add_list(response, board_id, name)
+@app.put('/lists/board/<token>/<board_id>/<name>')
+def add_list_to_board(token, board_id, name):
+    userdata = extract_userdata(token)
+    return add_list(response, userdata["uuid"], board_id, name)
 
 
-@app.delete('/lists/board/<board_id>')
-def remove_lists(board_id):
-    return remove_lists_for_board(response, board_id)
+@app.delete('/lists/board/<token>/<board_id>')
+def remove_lists(token, board_id):
+    userdata = extract_userdata(token)
+    return remove_lists_for_board(response, userdata["uuid"], board_id)
 
 
-def lists_for_board(response, board_id):
+def extract_userdata(token):
+    return jwt.decode(token, config['jwt']['secret'], algorithms=[config['jwt']['algorithm']])
+
+
+def lists_for_board(response, owner, board_id):
     json_content(response)
     json_lists = []
 
     with dataset.connect(LOCATION_DATA) as db:
         db.begin()
         list_table = db['list']
-        lists = list_table.find(board_id=board_id)
+        lists = list_table.find(board_id=board_id, owner=owner)
 
         for row in lists:
-            new_list = List(row['list_id'], row['board_id'], row['name'])
+            new_list = List(row['list_id'], row['board_id'], row['name'], row['owner'])
             json_lists.append(new_list)
 
     return json.dumps([l.dump() for l in json_lists])
 
 
-def add_list(response, board_id, name):
+def add_list(response, owner, board_id, name):
     json_content(response)
     list_id = uuid.uuid4()
-    new_list = List(str(list_id), str(board_id), name)
+    new_list = List(str(list_id), str(board_id), name, str(owner))
 
     with dataset.connect(LOCATION_DATA) as db:
         db.begin()
@@ -77,13 +88,13 @@ def add_list(response, board_id, name):
     return json.dumps(new_list.dump())
 
 
-def remove_list(response, list_id):
+def remove_list(response, owner, list_id):
     json_content(response)
 
     with dataset.connect(LOCATION_DATA) as db:
         db.begin()
         try:
-            db['list'].delete(list_id=list_id)
+            db['list'].delete(list_id=list_id, owner=owner)
             db.commit()
         except:
             db.rollback()
@@ -91,13 +102,13 @@ def remove_list(response, list_id):
     return json.dumps({'deleted': True})
 
 
-def remove_lists_for_board(response, board_id):
+def remove_lists_for_board(response, owner, board_id):
     json_content(response)
 
     with dataset.connect(LOCATION_DATA) as db:
         db.begin()
         try:
-            db['list'].delete(board_id=board_id)
+            db['list'].delete(board_id=board_id, owner=owner)
             db.commit()
         except:
             db.rollback()
