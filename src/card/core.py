1b1e7f20dccc69ff481d96d509ca147fb0461c4d
Jens Sproede <jens.sproede@gmail.com>
2016-08-29 08:50:55 +0200

    Card-Service: Added some logging

--- src/card/core.py
+++ src/card/core.py
@@ -1,10 +1,10 @@
 import configparser
 import json
-import os
+import logging
 import uuid
 import dataset
 
-from bottle import Bottle, run, response, request
+from bottle import Bottle, run, response, request, HTTPResponse
 from jose import jwt
 
 
@@ -13,6 +13,7 @@ config.read('card.ini')
 
 LOCATION_DATA = config['card']['sqlite_connect']
 
+logging.basicConfig(filename=config['card']['logfile'], level=logging.DEBUG)
 
 class Card(object):
     def __init__(self, list_id, card_id, name, description, owner):
@@ -42,11 +43,12 @@ def get_all_cards_for_list(token, list_id):
 
 @app.delete('/cards/<token>/<card_id>')
 def remove(token, card_id):
+    logging.debug("Deleting card with id %s", card_id)
     userdata = extract_userdata(token)
     return remove_card(response, userdata["user_id"], card_id)
 
 
-@app.put('/cards/<token>/<list_id>')
+@app.post('/cards/<token>/<list_id>')
 def add(token, list_id):
     userdata = extract_userdata(token)
 
@@ -65,14 +67,20 @@ def extract_userdata(token):
 
 
 def add_card(response, owner, list_id, name, description):
+    logging.debug("Adding card list_id=%s name=%s desc=%s", list_id, name, description)
     json_content(response)
     card_id = uuid.uuid4()
     new_card = Card(str(list_id), str(card_id), name, description, owner)
 
     with dataset.connect(LOCATION_DATA) as db:
-        db.begin()
-        db['card'].insert(new_card.__dict__)
-        db.commit()
+        try:
+            db.begin()
+            db['card'].insert(new_card.__dict__)
+            db.commit()
+            logging.debug("Card has been added")
+        except:
+            logging.debug("Card could not be added")
+            return HTTPResponse(status=500)
 
     return json.dumps(new_card.dump())
 
@@ -112,4 +120,4 @@ def json_content(response):
 
 
 if __name__ == '__main__':
-    run(app, host=config['card']['host'], port=config['card']['port'], debug=True)
+    run(app, host=config['card']['host'], port=config['card']['port'], debug=True, server='cherrypy')
