d6adb188d5a7833fec20990cb1ebf86ec5ce9e4a
René Leban <leban.rene@gmail.com>
2016-09-30 07:01:02 +0200

    add card-count to card service

--- src/card/core.py
+++ src/card/core.py
@@ -7,13 +7,13 @@ import dataset
 from bottle import Bottle, run, response, request, HTTPResponse
 from jose import jwt
 
-
 config = configparser.ConfigParser()
 config.read('card.ini')
 
 LOCATION_DATA = config['card']['sqlite_connect']
 
 logging.basicConfig(filename=config['card']['logfile'], level=logging.DEBUG)
+
 
 class Card(object):
     def __init__(self, list_id, card_id, name, description, owner):
@@ -38,42 +38,59 @@ app = Bottle()
 
 @app.get('/cards/<token>/<list_id>')
 def get_all_cards_for_list(token, list_id):
-    return cards_for_list(response, list_id)
+    json_content()
+    json_cards = []
+
+    with dataset.connect(LOCATION_DATA) as db:
+        db.begin()
+        card_table = db['card']
+        cards = card_table.find(list_id=list_id)
+
+        for row in cards:
+            new_card = Card(row['list_id'], row['card_id'], row['name'], row['description'],
+                            row['owner'])
+            json_cards.append(new_card)
+
+    return json.dumps([l.dump() for l in json_cards])
+
+
+@app.get('/cards/count/<token>/<list_id>')
+def count_card_in_list(token, list_id):
+    json_content()
+
+    with dataset.connect(LOCATION_DATA) as db:
+        db.begin()
+        card_table = db['card']
+        count = card_table.count(list_id=list_id)
+
+    return json.dumps({'count': count})
 
 
 @app.delete('/cards/<token>/<card_id>')
 def remove(token, card_id):
     logging.debug("Deleting card with id %s", card_id)
-    userdata = extract_userdata(token)
-    return remove_card(response, userdata["user_id"], card_id)
+    user_data = extract_user_data(token)
+    owner = user_data["user_id"]
+
+    json_content()
+
+    with dataset.connect(LOCATION_DATA) as db:
+        db.begin()
+        try:
+            db['card'].delete(card_id=card_id, owner=owner)
+            db.commit()
+        except:
+            db.rollback()
+            return HTTPResponse(status=404)
+
+    return HTTPResponse(status=200)
 
 
 @app.delete('/cards/list/<token>/<list_id>')
 def remove_cards_for_list(token, list_id):
-    userdata = extract_userdata(token)
-    return remove_cards(response, userdata["user_id"], list_id)
-
-
-@app.post('/cards/<token>/<list_id>')
-def add(token, list_id):
-    userdata = extract_userdata(token)
-
-    forms = request.forms
-    name = forms.name
-    description = forms.description
-
-    if description is None:
-        description = ""
-
-    return add_card(response, userdata["user_id"], list_id, name, description)
-
-
-def extract_userdata(token):
-    return jwt.decode(token, config['jwt']['secret'], algorithms=[config['jwt']['algorithm']])
-
-
-def remove_cards(response, owner, list_id):
-    json_content(response)
+    user_data = extract_user_data(token)
+    owner = user_data["user_id"]
+    json_content()
     with dataset.connect(LOCATION_DATA) as db:
         db.begin()
         try:
@@ -86,9 +103,20 @@ def remove_cards(response, owner, list_id):
     return HTTPResponse(status=200)
 
 
-def add_card(response, owner, list_id, name, description):
+@app.post('/cards/<token>/<list_id>')
+def add(token, list_id):
+    user_data = extract_user_data(token)
+    owner = user_data["owner"]
+
+    forms = request.forms
+    name = forms.name
+    description = forms.description
+
+    if description is None:
+        description = ""
+
     logging.debug("Adding card list_id=%s name=%s desc=%s", list_id, name, description)
-    json_content(response)
+    json_content()
     card_id = uuid.uuid4()
     new_card = Card(str(list_id), str(card_id), name, description, owner)
 
@@ -105,40 +133,14 @@ def add_card(response, owner, list_id, name, description):
     return json.dumps(new_card.dump())
 
 
-def remove_card(response, owner, card_id):
-    json_content(response)
-
-    with dataset.connect(LOCATION_DATA) as db:
-        db.begin()
-        try:
-            db['card'].delete(card_id=card_id, owner=owner)
-            db.commit()
-        except:
-            db.rollback()
-            return HTTPResponse(status=404)
-
-    return HTTPResponse(status=200)
+def extract_user_data(token):
+    return jwt.decode(token, config['jwt']['secret'], algorithms=[config['jwt']['algorithm']])
 
 
-def cards_for_list(response, list_id):
-    json_content(response)
-    json_cards = []
-
-    with dataset.connect(LOCATION_DATA) as db:
-        db.begin()
-        card_table = db['card']
-        cards = card_table.find(list_id=list_id)
-
-        for row in cards:
-            new_card = Card(row['list_id'], row['card_id'], row['name'], row['description'], row['owner'])
-            json_cards.append(new_card)
-
-    return json.dumps([l.dump() for l in json_cards])
-
-
-def json_content(response):
+def json_content:
     response.content_type = 'application/json; charset=utf-8'
 
 
 if __name__ == '__main__':
-    run(app, host=config['card']['host'], port=config['card']['port'], debug=True, server='cherrypy')
+    run(app, host=config['card']['host'], port=config['card']['port'], debug=True,
+        server='cherrypy')
